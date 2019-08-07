200b703ccbac0c322735761e2bef3d6708a0c0e0
hackleman <hackleman.jason@gmail.com>
2019-08-07 15:57:48 -0500

    new db migration

--- models.py
+++ models.py
@@ -14,3 +14,21 @@ class Result(db.Model):
     
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+class Tab(db.Model):
+    __tablename__ = 'tabs'
+
+    id = db.Column(db.Integer, primary_key = True)
+    artist = db.Column(db.String())
+    title = db.Column(db.String())
+    url = db.Column(db.String())
+    lines = db.Column(JSON)
+
+    def __init__(self, url, result_all):
+        self.artist = artist
+        self.title = title
+        self.url = url
+        self.result_all = result_all
+    
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
