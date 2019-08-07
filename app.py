4418a22bfb848d1b6b8029566b0db5c3b563c903
hackleman <hackleman.jason@gmail.com>
2019-08-07 12:18:11 -0500

    db migrations

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import os
 import requests
 import json
 from flask import Flask, render_template, request, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+from models import Result
 
 @app.route('/')
 def index():
-    return render_template('index.html');
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
